e3734b9fdafd8559f6dd0284fd80fbd527984e4d
Aman Yadav <itsamanyadav18@gmail.com>
2024-07-11 04:27:05 +0530

    password authentication failed

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from './schema'
+import postgres from 'postgres';
+
+
+const queryClient = postgres("process.env.DATABASE_URL!");
+const db = drizzle(queryClient, {schema});
+
+export {db};
